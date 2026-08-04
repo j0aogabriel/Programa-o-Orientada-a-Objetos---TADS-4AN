751d59b38fe34083d14dc0e4bb2339c0dfd3874d
João Gabriel Reis dos Santos <reisdossantosjoaogabriel@gmail.com>
2026-08-04 20:18:35 -0300

    Add files via upload

---
+++ programas/src/aulas/Aula2.java
@@ -0,0 +1,21 @@
+public class Aula2 { //criacao da classe - nome classe sempre inicia em maiusculo
+
+    public static void main(String[] args) { //metodo principal
+        
+        // Declaração de variáveis
+        String nome = "Maria"; //textos numeros e simbolos
+        int idade = 20; //numeros inteiros
+        double altura = 1.68; //numeros extensos com casas decimais 36 digitos
+        char sexo = 'F'; //guarda caracteres
+        boolean estudante = true; //guarda valores booleanos (true or false)
+
+        // Exibição das variáveis
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Altura: " + altura + " m");
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Estudante: " + estudante);
+
+
+    }
+}
